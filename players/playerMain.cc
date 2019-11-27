0d3817297f03ab0859ac7710600e2d127f05ec08
hoge <p50p100@yahoo.co.jp>
2019-11-27 09:14:30 +0900

    Bug Fix: Process is hungout or dump core. I modified not checking on head of loops, but checking after reading input.

--- players/playerMain.cc
+++ players/playerMain.cc
@@ -1,8 +1,10 @@
 #include "players.hh"
 
 int main(int argc, char* argv[]) {
-  while (!cin.eof()) {
+  while (true) {
     GameInfo info(cin);
+    if(cin.eof() )
+      break;
     int plan = info.id < 2 ? planSamurai(info) : planDog(info);
     cout << plan << endl;
   }
